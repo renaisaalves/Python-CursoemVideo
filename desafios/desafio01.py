e9157f2be4f3dc74ea94134ff9fe388dfa4d3315
Renaísa <renaisaalves@gmail.com>
2021-12-20 16:57:23 -0300

    Atualização dos Documentos

---
+++ desafios/desafio01.py
@@ -0,0 +1,2 @@
+nome = input('Qual é o seu nome? ')
+print('Olá,', nome+'! Seja muito bem vindo(a) ao curso de Python promovido pelo Curso em Vídeo.')
---
+++ desafios/desafio03.py
@@ -0,0 +1,4 @@
+n1 = input('Primeiro número: ')
+n2 = input('Segundo número: ')
+resultado = n1 + n2
+print('A soma desses dois números é = ',resultado)
