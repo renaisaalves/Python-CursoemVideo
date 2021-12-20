c56119ad6801fe610fe32fe1af16e09947fd527b
Renaísa <renaisaalves@gmail.com>
2021-12-20 17:46:43 -0300

    Updates

--- exercicios/ex001.py
+++ exercicios/ex001.py
@@ -1,9 +1,5 @@
-print('Olá, Mundo!')
 nome = input('Qual é o seu nome? ')
-print('Olá,', nome+'! Prazer em conhecê-lo(a).\n')
-print('Eu gostaria que você me informasse alguns dados, ok? Vamos lá!\n')
-print('Escreva o dia, mês e ano do seu nascimento:\n')
-dia = input('Dia: ')
-mes = input('Mês: ')
-ano = input('Ano: ')
-print('A data do seu aniversário é dia', dia,'de', mes,'de', ano,'('+dia+'/'+mes+'/'+ano+').')
+print('Olá,', nome+'! Seja muito bem vindo(a) ao curso de Python promovido pelo Curso em Vídeo.')
+
+msg='Olá, Mundo!'
+print(msg)
