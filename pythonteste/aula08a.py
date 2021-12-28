ee9c1e18b696ec74c00e0e954a7238ae259371b0
Renaísa <renaisaalves@gmail.com>
2021-12-28 14:24:50 -0300

    Update aula08a.py

--- pythonteste/aula08a.py
+++ pythonteste/aula08a.py
@@ -9,3 +9,5 @@ print('A raiz de um {} é igual a {}' .format(num, raiz))
 import emoji
 print(emoji.emojize("Olá, Mundo! :heart:", use_aliases=True))
 
+#Sempre que eu quiser instalar uma biblioteca, basta ir no TERMINAL e escrever pip install nome da biblioteca
+#Caso queira saber a lista de bibliotecas já instaladas, basta digitar pip list no TERMINAL. 
