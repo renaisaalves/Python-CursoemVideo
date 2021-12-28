a98e262c24ab98fb34588b3933f36bb4b8869dd5
Renaísa <renaisaalves@gmail.com>
2021-12-28 14:14:16 -0300

    Create aula08a.py

---
+++ pythonteste/aula08a.py
@@ -0,0 +1,11 @@
+#AULA 8: USANDO MÓDULOS DO PYTHON
+
+import math
+
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print('A raiz de um {} é igual a {}' .format(num, raiz))
+
+import emoji
+print(emoji.emojize("Olá, Mundo! :heart:", use_aliases=True))
+
