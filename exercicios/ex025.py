fb2c3d0d7c97054b727c5ea97431281504a34d27
Renaísa <renaisaalves@gmail.com>
2022-01-05 14:24:19 -0300

    Update ex025.py

--- exercicios/ex025.py
+++ exercicios/ex025.py
@@ -1,4 +1,5 @@
 #ex025: Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.
 
-nome = str(input('Digite seu nome completo:\n' )).strip
-print('Seu nome tem Silva? {}' .format('Silva' in nome))
+nome = str(input('Digite seu nome completo: ')).strip
+print('Seu nome tem Silva?')
+print('Silva' in nome)
