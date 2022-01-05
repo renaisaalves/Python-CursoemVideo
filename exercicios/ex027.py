5e1c0963e681192e5f3aabc9a88b6d4d4a7cf391
Renaísa <renaisaalves@gmail.com>
2022-01-05 16:41:44 -0300

    Update ex027.py

--- exercicios/ex027.py
+++ exercicios/ex027.py
@@ -1,5 +1,10 @@
 #ex027: Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
 
-nome = str(input('Digite seu nome completo: ')).strip()
+n = str(input('Digite seu nome completo: ')).strip().title()
 print('Muito prazer em te conhecer!')
-print('Seu primeiro nome é {}\n Seu último nome é {}.' .format())
+nome = n.split()
+print('Seu primeiro nome é {}.' .format(nome[0]))
+print('Seu último nome é {}'.format(nome[-1]))
+
+#Importante: quando usamos split (lista) o programa conta por frase e não por palavra. Assim, 'Renaisa' 'Alves' 'Silva' possui 3 posições (0, 1, 2). Ao colocarmos 0, estaremos sempre indicando a primeira frase da lista.
+#A dica de ouro começa agora: sempre que quiser usar a última frase/elemento da lista, coloque em número negativo. Assim, o programa começará a ler da direita para a esquerda. 
