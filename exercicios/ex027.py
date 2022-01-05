8db6d54f249fa0f897d8652e424fa7dd22acada6
Renaísa <renaisaalves@gmail.com>
2022-01-05 16:08:55 -0300

    Create ex027.py

---
+++ exercicios/ex027.py
@@ -0,0 +1,5 @@
+#ex027: Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Muito prazer em te conhecer!')
+print('Seu primeiro nome é {}\n Seu último nome é {}.' .format())
