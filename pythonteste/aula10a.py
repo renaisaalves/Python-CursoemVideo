2c9197e1e9a3767791128559a2490600897440f5
Renaísa <renaisaalves@gmail.com>
2022-01-06 17:45:16 -0300

    Update aula10a.py

--- pythonteste/aula10a.py
+++ pythonteste/aula10a.py
@@ -1,4 +1,4 @@
-#AULA 10: CONDIÇÃO
+#AULA 10: CONDIÇÃO SIMPLES E COMPOSTA
 
 tempo = int(input('Quantos anos tem seu carro? '))
 if tempo <= 10:
@@ -16,9 +16,17 @@ print('--FIM--')
 #Mas não gosto desse método. Prefiro o primeiro.
 
 nome = str(input('Qual é o seu nome? ')).strip()
-if nome == 'Gustavo':
-    print('Seu nome é tão comum.')
-else: 
+if nome == 'Renaísa':
     print('Que nome lindo você tem!')
+else: 
+    print('Seu nome é tão comum.')
 print('Bom dia {}!' .format(nome))
 
+n1 = float(input('Digite sua primeira nota: '))
+n2 = float(input('Digite sua segunda nota: '))
+m = (n1 + n2)/2
+print('A sua média foi: {}' .format(m))
+if m >= 6.0:
+    print('Sua média foi boa! Parabéns')
+else:
+    print('Sua média foi ruim! Estude novamente')
