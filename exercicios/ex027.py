8b9421d073347b2e13dc80904317ea45a2ddd51c
Renaísa <renaisaalves@gmail.com>
2022-01-06 16:07:16 -0300

    Update ex027.py

--- exercicios/ex027.py
+++ exercicios/ex027.py
@@ -7,4 +7,4 @@ print('Seu primeiro nome é {}.' .format(nome[0]))
 print('Seu último nome é {}'.format(nome[-1]))
 
 #Importante: quando usamos split (lista) o programa conta por frase e não por palavra. Assim, 'Renaisa' 'Alves' 'Silva' possui 3 posições (0, 1, 2). Ao colocarmos 0, estaremos sempre indicando a primeira frase da lista.
-#A dica de ouro começa agora: sempre que quiser usar a última frase/elemento da lista, coloque em número negativo. Assim, o programa começará a ler da direita para a esquerda. 
+#A dica de ouro começa agora: sempre que quiser usar a última frase/elemento da lista, coloque em número negativo. Assim, o programa começará a ler da direita para a esquerda. No caso, [Renaisa] = -3 [Alves] = -2 [Silva] = -1 
