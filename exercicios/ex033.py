90c73c7fa2d0936f0ead0bd87a0377c23b5c9691
Renaísa <renaisaalves@gmail.com>
2022-01-08 11:09:11 -0300

    Create ex033.py

---
+++ exercicios/ex033.py
@@ -0,0 +1,5 @@
+#ex033: Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+n3 = int(input('Terceiro número: '))
