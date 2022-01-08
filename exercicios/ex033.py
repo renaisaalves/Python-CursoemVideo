d7e56d97180c60b7f946582489405a199f9a6b73
Renaísa <renaisaalves@gmail.com>
2022-01-08 11:41:25 -0300

    Update ex033.py

--- exercicios/ex033.py
+++ exercicios/ex033.py
@@ -3,3 +3,18 @@
 n1 = int(input('Primeiro número: '))
 n2 = int(input('Segundo número: '))
 n3 = int(input('Terceiro número: '))
+# Verificando quem é o menor
+menor = n1 
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+print('O menor número foi {}.' .format(menor))
+maior = n1 
+if n2 > n1 and n2 > n3:
+    maior = n2
+if n3 > n1 and n3 > n2:
+    maior = n3
+print('O maior número foi {}.' .format(maior))
+    
+     
