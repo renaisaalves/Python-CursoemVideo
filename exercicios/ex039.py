ec59a46733336d0243e27d67a11d1ab86b1334f9
Renaísa <renaisaalves@gmail.com>
2022-01-11 11:46:06 -0300

    Update ex039.py

--- exercicios/ex039.py
+++ exercicios/ex039.py
@@ -1,5 +1,7 @@
 #ex039: Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade, se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
 
+#FEITO POR MIM 
+
 from datetime import date
 
 nasc = int(input('Digite seu ano de nascimento: '))
