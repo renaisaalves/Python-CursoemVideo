504230f2d06d34c3c03ba5f84d12dc75c7e33602
Renaísa <renaisaalves@gmail.com>
2022-01-12 14:11:18 -0300

    Update ex040.py

--- exercicios/ex040.py
+++ exercicios/ex040.py
@@ -2,3 +2,14 @@
 #Média abaixo de 5.0: REPROVADO
 #– Média entre 5.0 e 6.9: RECUPERAÇÃO
 #– Média 7.0 ou superior: APROVADO
+
+n1 = float(input('Primeira nota: '))
+n2 = float(input('Segunda nota: '))
+media = (n1 + n2) / 2
+if media < 5.0:
+    print('Sua média foi {}. Portanto, você está {}REPROVADO{}.' .format(media, '\033[1;31m','\033[m'))
+elif media > 5.0 and media <= 6.9:
+    print('Sua média foi {}. Portanto, você está de {}RECUPERAÇÃO{}.' .format(media, '\033[1;33m','\033[m'))
+else:
+    media > 7.0 
+    print('Sua média foi {}. Portanto, você está {}APROVADO{}.' .format(media, '\033[1;32m','\033[m'))
