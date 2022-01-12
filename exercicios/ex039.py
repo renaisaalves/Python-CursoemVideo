6543d9fd0e71fbc896c249fbede0d80feecf9353
Renaísa <renaisaalves@gmail.com>
2022-01-12 13:54:36 -0300

    Update ex039.py

--- exercicios/ex039.py
+++ exercicios/ex039.py
@@ -11,14 +11,14 @@ idade = atual - nasc
 if idade < 18:
     diferenca = 18 - idade 
     falta = atual + diferenca
-    print('De acordo com a sua idade ({}), você ainda vai se alistar no Serviço Militar.' .format(idade))
+    print('De acordo com a sua idade ({}), você ainda vai se alistar.' .format(idade))
     print('Falta(m) {} ano(s) para você se alistar.' .format(diferenca))
     print('O seu alistamento está previsto para ser em \033[1;32m{}\033[m.' .format(falta))
 elif idade == 18:
-    print('De acordo com a sua idade ({}), já está na hora exata de se alistar no Serviço Militar.' .format(idade))
+    print('De acordo com a sua idade ({}), já está na hora exata de se alistar.' .format(idade))
 else:
     diferenca = idade - 18
     falta = atual - diferenca
-    print('De acordo com a sua idade ({}), já passou do tempo do seu alistamento no Serviço Militar.' .format(idade))
+    print('De acordo com a sua idade ({}), já passou do tempo do seu alistamento.' .format(idade))
     print('A idade para se alistar é de 18 anos. Já se passou \033[1;31m{}\033[m ano(s) que você não se alistou.' .format(diferenca))
     print('Seu alistamento deveria ter sido em \033[1;31m{}\033[m.' .format(falta))
