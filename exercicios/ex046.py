99fccada8e8c2baed6284348a6516b5ac945feff
Renaísa <renaisaalves@gmail.com>
2022-01-20 15:10:54 -0300

    Create ex046.py

---
+++ exercicios/ex046.py
@@ -0,0 +1,14 @@
+#ex046: Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+
+from time import sleep
+from random import randint
+import emoji
+
+for c in range(10, 0, -1):
+    print(c)
+    sleep(1)
+print('*' * 20)
+print(emoji.emojize(':sparkler: FELIZ ANO NOVO!!!:sparkles:'))
+print('*' * 20)
+
+
