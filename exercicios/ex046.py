acb82745479a799101419fb55289361f396faf7f
Renaísa <renaisaalves@gmail.com>
2022-01-21 16:23:42 -0300

    Update ex046.py

--- exercicios/ex046.py
+++ exercicios/ex046.py
@@ -4,7 +4,7 @@ from time import sleep
 from random import randint
 import emoji
 
-for contagem in range(10, 0, -1):
+for contagem in range(10, -1, -1):
     print(contagem)
     sleep(1)
 print('*' * 20)
