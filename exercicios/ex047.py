496cce636648e883d0bbec99c142baa7ef0b375e
Renaísa <renaisaalves@gmail.com>
2022-01-21 16:36:34 -0300

    Update ex047.py

---
+++ exercicios/ex047.py
@@ -0,0 +1,6 @@
+#ex047: Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+
+for c in range(1, 51):
+    if c % 2 == 0:
+        print('{}' .format(c))
+print('Esses são todos os números pares!')
