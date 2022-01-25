ed0aa2382cccfb842a2e1495c15971e83125f91b
Renaísa <renaisaalves@gmail.com>
2022-01-25 18:09:46 -0300

    Create ex055.py

---
+++ exercicios/ex055.py
@@ -0,0 +1,9 @@
+#ex055: Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
+
+for c in range(1, 6):
+    peso = float(input('Informe o peso da {}ª pessoa: ' .format(c)))                
+        
+#print('O maior peso é {} e o menor peso é {}' .format(maior, menor))
+# 44.8 > 0 true 
+# 44.8 = 44.8 
+#50 > 44.8 true
