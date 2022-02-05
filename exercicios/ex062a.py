826699bccfd9bc5fbc2f4a10fea55ee1e6e75e70
Renaísa <renaisaalves@gmail.com>
2022-02-05 16:50:35 -0300

    Create ex062a.py

---
+++ exercicios/ex062a.py
@@ -0,0 +1,25 @@
+#62: Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerrará quando ele disser que quer mostrar 0 termos.
+
+#FEITO POR MIM
+
+from multiprocessing import Condition
+
+
+a1 = int(input('Primeiro termo: '))
+r = int(input('Razão: '))
+
+termo = 0
+while termo < 10:
+    termo = termo + 1
+    an = a1 + (termo - 1) * r
+    print(an, '➝ ', end=' ')
+    if termo == 10:
+        print('PAUSA')
+        novotermo = int(input('\nQuantos termos você quer mostrar a mais? '))
+        if novotermo != 0:
+            termo = novotermo + termo
+            print('')
+        else:
+            novotermo == 0
+            print('FIM DO PROGRAMA.')
+print('FIM DO PROGRAMA.')
