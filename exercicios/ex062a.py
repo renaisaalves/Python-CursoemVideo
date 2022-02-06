00b806623a3b51bc1b5f716242161d7f46677522
Renaísa <renaisaalves@gmail.com>
2022-02-06 17:34:58 -0300

    Update ex062a.py

--- exercicios/ex062a.py
+++ exercicios/ex062a.py
@@ -13,9 +13,18 @@ while termo < 10:
     if termo == 10:
         print('PAUSA')
         novotermo = int(input('\nQuantos termos você quer mostrar a mais? '))
-        if novotermo != 0:
-            termo = novotermo + termo
-            print('')
-        else:
+        while novotermo != 0:
+            novotermo2 = novotermo + termo
+            while novotermo2 > termo:
+                termo = termo + 1
+                an = a1 + (termo - 1) * r
+                print(an, '➝ ', end=' ')
+                if novotermo2 == termo:
+                    print('FIM.')
+                    novotermo = int(input('\nQuantos termos você quer mostrar a mais? '))
+        else: 
             novotermo == 0
 print('FIM DO PROGRAMA.')
+
+#novotermo vai definir se o usuário quer continuar ou não exibindo os termos. 
+#novotermo receberá o valor de quantos termos ele quer mostrar a mais e o termo estará com a configuração anterior (10)
