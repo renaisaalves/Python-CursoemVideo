986620246bcdd2964f28e97efb0c933c78b30b79
Renaísa <renaisaalves@gmail.com>
2022-02-09 15:39:32 -0300

    Update ex063a.py

--- exercicios/ex063a.py
+++ exercicios/ex063a.py
@@ -10,7 +10,9 @@ termos = int(input('Quantos termos você quer mostrar? '))
 
 while contagem != termos:
     contagem = contagem + 1
-    resultado = valor1 + valor2
+    if contagem == 1:
+        resultado = valor1 + valor2
+    resultado = resultado + valor2
     print(resultado, '–', end=' ')
 print('FIM.')
 
