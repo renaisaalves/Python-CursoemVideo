88702e48a33f4286dd4c9a90b0a72c1839466cec
Renaísa <renaisaalves@gmail.com>
2022-02-11 16:31:22 -0300

    Update ex064a.py

--- exercicios/ex064a.py
+++ exercicios/ex064a.py
@@ -1,5 +1,7 @@
 #64: Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag).
 
+#FEITO POR MIM
+
 soma = 0
 n2 = 0
 n = 0
