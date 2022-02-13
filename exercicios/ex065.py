237010163581c69a8c478104b3b786fc3ace0f75
Renaísa <renaisaalves@gmail.com>
2022-02-13 17:45:46 -0300

    Update ex065.py

--- exercicios/ex065.py
+++ exercicios/ex065.py
@@ -1,22 +1,34 @@
 ##65: Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
 
+contador = 0
+resposta = 'SIM' or 'S'
+
 print('==' * 8)
 print('CALCULE A MÉDIA')
 print('==' * 8)
 
 contador = 0
 resposta = 'SIMsSsim'
-
+soma = media = 0
 while resposta in 'SIMsSsim': 
-    numero = int(input('Digite outro número: '))
     contador = contador + 1 
+    if contador == 1: 
+        numero = int(input('Digite um número: '))
+        maior = menor = numero 
+    numero = int(input('Digite outro número: '))
+    if numero > maior:
+        maior = numero
+    else: 
+        menor > numero
+        menor = numero
     resposta = str(input('Quer continuar? '))
-    if resposta != 'SIMsSsim' and 'NãoNÃOnNnaoNAO':
-        resposta = print('Resposta inválida. Digite [sim/não]: ')
     if resposta in 'NãoNÃOnNnaoNAO':
+        print('=' * 40)
         print('TOTAL de números digitados: {}' .format(contador))
         print('SOMA dos números: {}' .format(soma))
         print('MÉDIA dos números: {}' .format(media))
         print('MAIOR valor: {}' .format(maior))
         print('MENOR valor: {}' .format(menor))
+        print('=' * 40)
 print('Fim do programa')
+
