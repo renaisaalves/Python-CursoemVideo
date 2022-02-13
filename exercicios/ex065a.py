53645389559c92b5a4fc3bfdc05e37878a348a04
Renaísa <renaisaalves@gmail.com>
2022-02-13 12:55:07 -0300

    Update ex065a.py

--- exercicios/ex065a.py
+++ exercicios/ex065a.py
@@ -1,50 +1,36 @@
 #65: Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
 
-print('=~' * 8)
+print('==' * 8)
 print('CALCULE A MÉDIA')
-print('=~' * 8)
+print('==' * 8)
 
-contador = 0 
-resposta = 'NULO'
-while contador != 2: 
-    contador = contador + 1
-    n = int(input('Digite um número: '))
-    if contador == 1:
-        m1 = n
-    if contador == 2:
-        m2 = n
-        if m1 > m2:
-            maior = m1 
-            menor = m2
-        else:
-            m2 > m1
-            maior = m2
-            menor = m1
-            soma = maior + menor
-            media = soma / 2
-        resposta = str(input('Você quer digitar mais algum número? [SIM/NÃO]: ')).strip()    
-        while resposta in 'SIMsimSs':
-            n = int(input('Digite mais um número: '))
-            contador = contador + 1
-            if n > maior: 
-                maior = n 
-            else: 
-                n < menor
-                menor = n
-                soma = maior + menor 
-                media = soma / contador
-                resposta = str(input('Você quer digitar mais algum número? [SIM/NÃO]: ')).strip()
-        else: 
-            print('=' * 40)
-            print('A MÉDIA entre os {} valores informados foi {:.2f}' .format(contador, media))
-            print('=' * 40)
-            print('O MAIOR valor é {} e o MENOR valor é {}' .format(maior, menor))
-            print('=' * 40)
-print('FIM DO PROGRAMA.')
-            
-#Para que a média aconteça, é necessário, no mínimo, de dois números para serem somados e divididos. 
-#A minha ideia inicial é que o usuário digite dois números e o programa já deixe pronto todas as informações que ele precisa (média e qual foi o menor e maior número informado.)
-#Se o usuário quiser continuar informando mais valores (acima de 2), vou adicionar uma estrutura de repetição que faça a mesma pergunta a cada valor digitado, pois é imprevisível saber quantos números o usuário quer. 
-#A soma possui a seguinte estrutura: soma = n1 + n2 
-#A média possui a seguinte estrutura: media = soma / contador (O contador vai fazer a contagem dos elementos informados)
-#A comparação de < e > possui a seguinte estrutura: a primeira repetição vai armazenar o primeiro valor e a segunda repetição vai comparar o novo valor com o valor anterior. A partir disso, tudo estará pronto para ser exercido no próximo while.
+contador = 0
+resposta = ' '
+
+valor = int(input('Digite um número: '))
+contador = contador + 1 
+m1 = valor
+valor = int(input('Digite outro número: '))
+contador = contador + 1 
+m2 = valor
+soma = m1 + m2 
+media = soma / contador 
+if m1 > m2:
+    maior = m1 
+    menor = m2 
+else: 
+    maior = m2 
+    menor = m1 
+print('TOTAL de números digitados: {}' .format(contador))
+print('SOMA dos números: {}' .format(soma))
+print('MÉDIA dos números: {:.2f}' .format(media))
+print('MAIOR número: {}' .format(maior))
+print('MENOR número: {}' .format(menor))
+
+total = 0 
+contador = int(input('Olá! Quantos números você deseja informar? '))
+while total != contador: 
+    total = total + 1 
+    valor = int(input('Digite o {}º número: ' .format(total)))
+    #resposta = str(input('Deseja inserir mais algum número? '))
+print('TOTAL de números digitados: {}' .format(total))
