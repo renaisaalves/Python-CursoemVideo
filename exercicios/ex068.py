d9eb3106c27b49b5a94c153fc30c1a4544a82d2b
Renaísa <renaisaalves@gmail.com>
2022-02-16 17:01:44 -0300

    Update ex068.py

--- exercicios/ex068.py
+++ exercicios/ex068.py
@@ -1,1 +1,5 @@
-#ex68: 
+#ex68: Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o jogador perder, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
+
+# vai haver uma soma (meu número com o número do computador), portanto, vamos importar o módulo randint da biblioteca random. 
+# Minha condição de parada (break) será quando o jogador perder. 
+# se o resto do número % == 0: par | senão, será ímpar. 
