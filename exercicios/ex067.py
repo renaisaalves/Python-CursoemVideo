13b6c6edf42c4f0c3c54d78f55c7ae87222edbd4
Renaísa <renaisaalves@gmail.com>
2022-02-16 15:20:58 -0300

    Update ex067.py

--- exercicios/ex067.py
+++ exercicios/ex067.py
@@ -1,1 +1,14 @@
 #67: Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.
+
+print('=' * 20)
+print('TABUADA EFICIENTE')
+print('=' * 20)
+
+while True:
+    numero = int(input('Digite um número: '))
+    if numero < 0:
+        break
+    for contagem in range(0, 11):
+        resultado = numero * contagem
+        print(f'{numero} x {contagem} = {resultado}')
+print('Fim do programa.')
