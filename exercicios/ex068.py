f592f976375b99b7fbff48d5d50ad628a9831fc7
Renaísa <renaisaalves@gmail.com>
2022-02-17 17:03:11 -0300

    Update ex068.py

--- exercicios/ex068.py
+++ exercicios/ex068.py
@@ -10,21 +10,22 @@ while True:
     escolha = str(input('PAR ou IMPAR? ')).upper()
     computador = randint(0, 5)
     if escolha == PAR:
-        computador == IMPAR
+        escolhapc = IMPAR
     else:
-        computador == PAR
+        escolhapc = PAR
     soma = jogador + computador 
     if soma % 2 == 0:
-        soma == PAR 
+        resultado = PAR 
     else: 
-        soma == IMPAR 
-    if soma and escolha == PAR:
+        resultado = IMPAR 
+    if escolha == resultado:
         print('Você ganhou!')
         vitoria = vitoria + 1 
     else:
         print('Você perdeu!')
         break
-print(f'A soma entre {jogador} + {computador} = {soma}, resultando em {escolha}!')
+print(f'Você escolheu {escolha} e o computador escolheu {escolhapc}.')
+print(f'A soma entre {jogador} + {computador} = {soma}, resultando em {resultado}!')
 print(f'Você teve {vitoria} vitórias.')
     
         
