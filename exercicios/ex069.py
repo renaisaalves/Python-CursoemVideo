f4b25f4876b18f6e57f36bd2a386170f655d6043
Renaísa <renaisaalves@gmail.com>
2022-02-18 12:46:09 -0300

    Update ex069.py

--- exercicios/ex069.py
+++ exercicios/ex069.py
@@ -3,3 +3,16 @@
 # quantos homens foram cadastrados.
 # quantas mulheres tem menos de 20 anos.
 
+while True:
+    idade = int(input('Informe uma idade: '))
+    while idade < 0 or idade > 200: 
+        idade = int(input('Inválido. Informe uma idade real: '))
+    sexo = str(input('Informe o gênero [F/M]: ')).strip().upper()
+    while sexo not in 'F/M':
+        sexo = str(input('Inválido. Informe o gênero novamente [F/M]: ')).strip().upper()
+    resposta = str(input('Deseja continuar? [Sim/Não]: '))
+    if resposta not in 'NãonaonNAONÃOnãoSimsimsSIMss':
+        resposta = str(input('Informação inválida. Digite [Sim/Não]: '))
+    if resposta in 'NãonaonNAONÃOnão':
+        break
+print('Deu certo até aqui.')
