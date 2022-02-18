07380f52aa635b5a1b0fd61ba69a8f2e7effa12c
Renaísa <renaisaalves@gmail.com>
2022-02-18 12:28:36 -0300

    Update ex068.py

--- exercicios/ex068.py
+++ exercicios/ex068.py
@@ -45,5 +45,29 @@ print(f'A soma entre {jogador} + {computador} = {soma}, {soma} é um número {re
 print(f'No total, você teve {vitoria} vitória(s).')
     
 #FEITO PELO GUANABARA
-
-
+v = 0
+while True:
+    jogador = int(input('Digite um valor: '))
+    computador = randint(0, 10)
+    total = jogador + computador
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('Par ou Ímpar? [P/I] ')).strip().upper()[0]
+    print(f'Você jogou {jogador} e o computador jogou {computador}. O total foi {total}', end='')
+    print('DEU PAR' if total % 2 == 0 else 'DEU ÍMPAR')
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('VOCÊ VENCEU!')
+            v = v + 1 
+        else:
+            print('VOCÊ PERDEU!')
+            break
+    elif tipo == 'I':
+        if total % 2 == 0:
+            print('VOCÊ VENCEU!')
+            v = v + 1 
+        else:
+            print('VOCÊ PERDEU!')
+            break
+    print('Vamos jogar novamente...')
+print(f'GAME OVER! Você venceu {v} vezes.')
