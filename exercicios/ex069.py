f4d269515c2b5602a93c78234aa0864c246cb31d
Renaísa <renaisaalves@gmail.com>
2022-02-18 12:33:38 -0300

    Create ex069.py

---
+++ exercicios/ex069.py
@@ -0,0 +1,5 @@
+#ex069: Crie um programa que leia a idade e o sexo de várias pessoas. A cada pessoa cadastrada, o programa deverá perguntar se o usuário quer ou não continuar. No final, mostre:
+# quantas pessoas tem mais de 18 anos.
+# quantos homens foram cadastrados.
+# quantas mulheres tem menos de 20 anos.
+
