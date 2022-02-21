36a257159a2ae780dc0644d684b25c68f18be1a8
Renaísa <renaisaalves@gmail.com>
2022-02-21 15:32:41 -0300

    Update ex070.py

--- exercicios/ex070.py
+++ exercicios/ex070.py
@@ -34,3 +34,25 @@ print('=' * 45)
 
 #FEITO POR GUANABARA
 
+total = totmil = menor = cont = 0
+while True:
+    produto = str(input('Nome do produto: '))
+    preço = float(input('Preço (R$): '))
+    cont += 1
+    total += preço
+    if preço > 1000:
+        totmil += 1
+    if cont == 1:
+        menor = preço
+    else:
+        if preço < menor:
+            menor = preço
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print('{:^40}'.format('FIM DO PROGRAMA.'))
+print(f'O total foi R${total:.2f}.')
+print(f'Temos {totmil} produtos custando mais de R$ 1000.')
+print(f'O produto mais barato custa R$ {menor:.2f}')
