bdf86ee1b69d5261f4e3cb1b8343102a84041e4a
Renaísa <renaisaalves@gmail.com>
2022-02-21 11:04:00 -0300

    Update ex070.py

--- exercicios/ex070.py
+++ exercicios/ex070.py
@@ -3,13 +3,20 @@
 #B) quantos produtos custam mais de R$1000.
 #C) qual é o nome do produto mais barato.
 
+contagem = total = 0 
 print('~' * 30)
-print('LOJA RA')
+print('LOJÃO RA')
 print('~' * 30)
-print(' ')
 while True:
+    produto = str(input('Informe o nome do produto: ')).strip().upper()
+    preço = float(input('Informe o preço do produto (R$): '))
+    if preço > 1000: 
+        contagem = contagem + 1 
+    total = total + preço 
     resposta = str(input('Quer continuar? ')).strip()
     if resposta in 'SimsimSIMsS':
         print('Testando')
     else:
         break
+print(f'O total gasto na compra foi: R${total:.2f}.')
+print(f'Ao todo, {contagem} produtos custam mais de R$ 1000.')
