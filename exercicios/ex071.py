8667037f7b4bf904a17393bedb301ee4281d9755
Renaísa <renaisaalves@gmail.com>
2022-02-22 14:11:45 -0300

    Update ex071.py

--- exercicios/ex071.py
+++ exercicios/ex071.py
@@ -4,12 +4,20 @@ um = 1
 dez = 10
 vinte = 20
 cinquenta = 50
+contagem = saque = 0
 
 while True:
     print('=' * 20)
     print('BANCO ORIGINAL RA')
     print('=' * 20)
     valor = int(input('Quanto você deseja sacar? R$: '))
+    while saque < valor:
+       if valor < 10:
+           saque = um + um 
+           contagem += 1
+        if valor == 10:
+            saque = dez or saque = 
     if valor == 0:
         break
+print(f'Saque de R${valor}:\n{contagem}cédulas de R${um}.\n{contagem} cédulas de R${dez}.\n{contagem}cédulas de R${vinte}.\n{contagem} cédulas de {cinquenta}.')
 print('Fim da operação.')
