51d41bd3a583c131d6df9909c6ce49a68b48c2b8
Renaísa <renaisaalves@gmail.com>
2022-02-22 16:15:33 -0300

    Update ex071.py

--- exercicios/ex071.py
+++ exercicios/ex071.py
@@ -1,21 +1,18 @@
 #ex071: Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues. OBS: considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
 
-um = 1
-dez = 10
-vinte = 20
-cinquenta = 50
-contagem = saque = 0
-
+cont50 = 0
 print('=' * 20)
 print('BANCO ORIGINAL RA')
 print('=' * 20)
 valor = int(input('Quanto você deseja sacar? R$: '))
+while valor > 50:
+    valor = valor - 50
+    cont50 = cont50 + 1
+    if valor < 50:
+        resto = valor
 print('-' * 15)
 print(f'Saque de R$ {valor}:')
 print('-' * 15)
-print(f'{contagem} cédulas de R${um}')
-print(f'{contagem} cédulas de R${dez}')
-print(f'{contagem} cédulas de R${vinte}')
-print(f'{contagem} cédulas de R${cinquenta}')
+print(f'{cont50} cédulas de R$50 e resto R${resto}.')
 print('-' * 15)
 print('Fim da operação.')
