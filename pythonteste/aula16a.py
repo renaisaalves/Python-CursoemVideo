03e0247a0672b401d9ac1cc55c6bbd35a14287de
Renaísa <renaisaalves@gmail.com>
2022-02-23 16:00:01 -0300

    Update aula16a.py

--- pythonteste/aula16a.py
+++ pythonteste/aula16a.py
@@ -1,6 +1,10 @@
 #AULA16 = TUPLAS ()
 lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim')
-print(lanche)
-print(lanche[0:3])
-print(lanche[3])
-print(lanche[-2])
+print(lanche) #exibe todas as posições da tupla
+print(lanche[0:3]) #começa na posição 0 [Hambúruger] e vai até a posição 2 [Pizza]
+print(lanche[3]) #exibe a posição 3 [Pizza]
+print(lanche[-2]) #conta da direita para a esquerda
+print(len(lanche)) #mostra quantas posições existem na variável
+
+for comida in lanche:
+    print(f'Eu vou comer {comida}.')
