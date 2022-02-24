2d012914268bfe981476302f43bdb096f26e83f5
Renaísa <renaisaalves@gmail.com>
2022-02-24 18:04:13 -0300

    Update ex072.py

--- exercicios/ex072.py
+++ exercicios/ex072.py
@@ -1,2 +1,7 @@
-#72: Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+#72: Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
 
+numeros = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
+
+for numero in numeros:
+    numero = int(input('Digite um número: '))
+print(f'O {numero} escrito por extenso é {numeros}')
