884fc4522ea5fd6f346c2ed5973fde76c62f5764
Renaísa <renaisaalves@gmail.com>
2022-02-24 18:07:59 -0300

    Update ex072.py

--- exercicios/ex072.py
+++ exercicios/ex072.py
@@ -3,3 +3,5 @@
 tupla = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
 
 numero = int(input('Digite um número: '))[0:21]
+
+#Eu tenho que assimilar o número digitado com a posição do item na memória. 
