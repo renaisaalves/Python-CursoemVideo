118b1b9a88088df7fcca0bd37e4bfb7dff267831
Renaísa <renaisaalves@gmail.com>
2022-02-25 15:22:05 -0300

    Update ex073.py

--- exercicios/ex073.py
+++ exercicios/ex073.py
@@ -1,1 +1,12 @@
-#ex073: 
+#ex073: Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. Depois mostre: a) Os 5 primeiros times. b) Os últimos 4 colocados. c) Times em ordem alfabética. d) Em que posição está o time da Chapecoense.
+
+print('='  * 40)
+print('CAMPEONATO BRASILEIRO DE FUTEBOL (2022)')
+print('='  * 40)
+
+rank = ('', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '')
+
+print(rank[0:6])
+print(rank[16:21])
+print(sorted(rank))
+print(rank.index())
