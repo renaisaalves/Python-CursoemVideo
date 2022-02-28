4d103b30c628b1f8b2c759e669fd43f52b63367a
Renaísa <renaisaalves@gmail.com>
2022-02-28 10:20:47 -0300

    Update ex074.py

--- exercicios/ex074.py
+++ exercicios/ex074.py
@@ -2,5 +2,9 @@
 
 from random import randint
 
-#NÃO CONSEGUI FAZER, PORQUE NÃO SEI INSERIR UM DADO E TRANSFORMAR EM TUPLA
+#NÃO CONSEGUI FAZER, PORQUE NÃO SEI COLOCAR DENTRO DE UMA TUPLA
 
+num = (58, 77, 18, 9, 16)
+print(sorted(num))
+print(f'Menor número: {num[0]}')
+print(f'Maior número: {num[4]}')
