4aa7a0ffae939c8bf2e16164886d13c43686b9fb
Renaísa <renaisaalves@gmail.com>
2022-03-01 11:47:45 -0300

    Update aula16a.py

--- pythonteste/aula16a.py
+++ pythonteste/aula16a.py
@@ -1,11 +1,11 @@
 #AULA16 = TUPLAS ()
-lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim')
+lanche = ('bolo', 'pastel', 'pizza', 'refrigerante', 'coxinha', 'brigadeiro')
 print(lanche) #exibe todas os elementos da tupla
-print(lanche[0:3]) #começa na posição 0 [Hambúruger] e vai até a posição 2 [Pizza]
-print(lanche[3]) #exibe a posição 3 [Pizza]
-print(lanche[-2]) #conta da direita para a esquerda
-print(len(lanche)) #mostra quantas posições existem na variável
-print(sorted(lanche)) #em ordem alfabética
+print(lanche[0:3]) #começa na posição 0 [bolo] e vai até a posição 2 [pizza]
+print(lanche[3]) #exibe a posição 3 [refrigerante]
+print(lanche[-2]) #conta da direita para a esquerda [coxinha -2, brigadeiro -1]
+print(len(lanche)) #mostra quantas posições existem na variável [6]
+print(sorted(lanche)) #se for números, em ordem crescente; se for letras, em ordem alfabética
 
 for c in lanche:
     print(f'Eu vou comer {c}.')
@@ -21,12 +21,10 @@ a = (2, 5, 7)
 b = (5, 8, 1, 2)
 c = a + b #vai juntar a tuplas a e b 
 print(c)
-d = b + a #vai juntar as tuplas b e a
-print(d)
 print(len(c))
-print(d.count(5)) #quantas vezes está aparecendo o número 5 dentro de d
-print(d.index(8)) #qual é a posição do número 8 (no caso, na tupla d)
-print(d.index(5, 1)) #se houver dois números iguais, o programa vai considerar a posição da primeira ocorrência. No entanto, caso você queira uma posição específica para um mesmo número/item (no caso o 5), você pode separar por vírgula o número que vc escolheu e a posição seguinte.
+print(c.count(5)) #quantas vezes está aparecendo o número 5 dentro de c
+print(c.index(8)) #qual é a posição do número 8 (no caso, na tupla c)
+print(c.index(5, 1)) #se houver dois números iguais, o programa vai considerar a posição da primeira ocorrência. No entanto, caso você queira uma posição específica para um mesmo número/item (no caso o 5), você pode separar por vírgula o número que vc escolheu e a posição seguinte.
 
 #Diferença entre JAVA e PYTHON.
 #No JAVA, por exemplo, vc só pode declarar um tipo, exemplo: pessoa = ('Maria', 'João', 'Totó') ou pessoa = (1, 2, 3). Já no PYTHON você pode declarar strings e números na mesma variável. Veja só:
