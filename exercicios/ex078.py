9904c4bfaeda2f3c56069e33e4696902354b0021
Renaísa <renaisaalves@gmail.com>
2022-03-03 16:10:50 -0300

    Update ex078.py

--- exercicios/ex078.py
+++ exercicios/ex078.py
@@ -1,19 +1,26 @@
 #ex078: Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
 
-#FEITO POR MIM 
+#NÃO CONSEGUI FAZER......
 
 valores = []
+maior = menor = 0
 
 for c in range(0, 5):
     valores.append(int(input(f'Digite o {c}º valor: ')))
-print(f'Valores digitados: {valores}')
-valores.sort()
-print(f'Menor valor: {valores[0]}')
-print(f'Maior valor: {valores[4]}')
-print(f'')
-
-#1º passo: especifiquei o tipo da minha variável;
-#2º passo: fiz um laço de repetição simples, do qual coleta 5 números do usuário;
-#3º passo: todos os números informados entram dentro de uma lista;
-#4º passo: faço o método de organizar os números [ordem crescente];
-#5º passo: com a lista já organizada em ordem crescente, é só inserir a primeira posição [0] e a última [4]
+    if c == 0:
+        maior = menor = valores[c]
+    else:
+        if valores[c] > maior:
+            maior = valores[c]
+        if valores[c] < menor:
+            menor = valores[c]
+print('=' * 30)
+print(f'Você digitou a lista {valores}')
+print(f'O maior valor digitado foi {maior} nas posições ', end='')
+for i, v in enumerate(valores):
+    if v == maior:
+        print(f'{i}...', end='')
+print(f'\nO menor valor digitado foi {menor} nas posições ', end='')
+for i, v in enumerate(valores):
+    if v == menor:
+        print(f'{i}...', end='')
