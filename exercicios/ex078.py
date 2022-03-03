83af10806e79419a166eaa442dafab6cbb25b389
Renaísa <renaisaalves@gmail.com>
2022-03-03 13:40:02 -0300

    Update ex078.py

--- exercicios/ex078.py
+++ exercicios/ex078.py
@@ -1,2 +1,12 @@
 #ex078: Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
 
+valores = []
+
+for c in range(1, 6):
+    valores.append(int(input(f'Digite o {c}º valor: ')))
+print(f'Valores digitados: {valores}')
+valores.sort()
+print(f'Menor valor: {valores[0]}')
+print(f'Maior valor: {valores[4]}')
+
+
