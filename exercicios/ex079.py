a6fb8d2a6510b8f6ecc054322e8cfbd8fcfbc412
Renaísa <renaisaalves@gmail.com>
2022-03-06 14:51:26 -0300

    Update ex079.py

--- exercicios/ex079.py
+++ exercicios/ex079.py
@@ -1,5 +1,7 @@
 #Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente.
 
+#NÃO CONSEGUI FAZER
+
 valores = []
 contador = 0
 i = '\033[1;33m'
@@ -19,7 +21,23 @@ while True:
         break
 print('=' * 30)
 print(f'Valores informados: {i}{valores}{l}')
-print(f'{num}')
 print('=' * 30)
 
 #tem que receber um valor individual e analisar se esse número existe na lista
+
+#FEITO POR GUANABARA
+
+numeros = []
+while True:
+    n = int(input('Digite um número: '))
+    if n not in numeros:
+        numeros.append(n)
+        print('Valor adicionado com sucesso.')
+    else:
+        print('Valor duplicado! Não vou adicionar.')
+    resposta = str(input('Quer continuar? [S/N] '))[0]
+    if resposta in 'Nn':
+        break
+print('=' * 30)
+numeros.sort()
+print(f'Você digitou os valores {numeros}')
