087af9cf8ce250809efb45258f7bad1da3b0d390
Renaísa <renaisaalves@gmail.com>
2022-03-14 19:19:16 -0300

    Update ex080.py

--- exercicios/ex080.py
+++ exercicios/ex080.py
@@ -4,6 +4,7 @@ lista = []
 
 for c in range(0, 3):
     val = int(input(f'Digite o {c}º valor: '))
+    lista(val).sort()
 print(f'{lista}')
 
 #Vamos supor que eu armazene um valor [4] em uma variável comum. Sabendo que a instrução está dentro de um laço, se eu não armazenar esse valor em uma lista ou em outra variável, na próximo laço eu vou perder esse valor. 
