241227c7314f2b63a1b246c1af039c2a4e43ffc5
Renaísa <renaisaalves@gmail.com>
2022-03-25 17:01:51 -0300

    Update ex081.py

--- exercicios/ex081.py
+++ exercicios/ex081.py
@@ -1,18 +1,18 @@
 #ex 081: Crie um programa que vai ler vários números e colocar em uma lista. Depois disso mostre: A) Quantos números foram digitados. B) A lista de valores, ordenada de forma decrescente. C) Se o valor 5 foi digitado e está ou não na lista.
 
 lista = []
-lista_true = ('O valor 5 foi digitado e está na lista.')
-lista_false = ('O valor 5 não foi digitado e não está na lista.')
 
 repeat = int(input('Quantas vezes você quer repetir? '))
 for c in range(1, repeat +1):
     number = int(input(f'Número {c}: '))
     lista.append(number)
-    if c == repeat:
-        if 5 in lista:
-            print(f'O valor 5 foi digitado e está na lista.')
-        else:
-            print(f'O valor 5 não foi digitado e não está na lista.')
+print('=' * 40)
 print(f'Sua lista: {lista}')
 print(f'Foram digitados {c} números.')
+print(f'Em ordem crescente: {lista.sort()}')
 print(f'Em ordem decrescente:', lista.sort(reverse=True))
+if 5 in lista:
+    print(f'O valor 5 foi digitado e está na lista.')
+else:
+    print(f'O valor 5 não foi digitado e não está na lista.')
+print('=' * 40)
