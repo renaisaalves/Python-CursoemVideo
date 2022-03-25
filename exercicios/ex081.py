5a570f3e3edfcfa7e2e9f45e380e7714b6cd8da3
Renaísa <renaisaalves@gmail.com>
2022-03-25 16:54:38 -0300

    Update ex081.py

--- exercicios/ex081.py
+++ exercicios/ex081.py
@@ -6,6 +6,11 @@ repeat = int(input('Quantas vezes você quer repetir? '))
 for c in range(1, repeat +1):
     number = int(input(f'Número {c}: '))
     lista.append(number)
+    if c == repeat:
+        if 5 in lista:
+            print(f'O valor 5 foi digitado e está na lista.')
+        else:
+            print(f'O valor 5 não foi digitado e não está na lista.')
 print(f'Sua lista: {lista}')
 print(f'Foram digitados {c} números.')
 print(f'Em ordem decrescente:', lista.sort(reverse=True))
