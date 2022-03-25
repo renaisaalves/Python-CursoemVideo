a9d729510997d09980746af8b2a1aa0a2864aa84
Renaísa <renaisaalves@gmail.com>
2022-03-25 16:45:41 -0300

    Update ex081.py

--- exercicios/ex081.py
+++ exercicios/ex081.py
@@ -1,1 +1,9 @@
 #ex 081: Crie um programa que vai ler vários números e colocar em uma lista. Depois disso mostre: A) Quantos números foram digitados. B) A lista de valores, ordenada de forma decrescente. C) Se o valor 5 foi digitado e está ou não na lista.
+
+lista = []
+
+repeat = int(input('Quantas vezes você quer repetir? '))
+for c in range(1, repeat +1):
+    number = int(input(f'Número {c}: '))
+    lista.append(number)
+print(lista)
