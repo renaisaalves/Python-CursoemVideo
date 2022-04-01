245380d7f4778fbb61658c2243bb5745e1a7fb72
Renaísa <renaisaalves@gmail.com>
2022-04-01 18:33:41 -0300

    Update ex082.py

--- exercicios/ex082.py
+++ exercicios/ex082.py
@@ -4,3 +4,4 @@
 #Não é porque não amo, é porque precisei abdicar de algumas coisas para conseguir alcançar um objetivo de médio prazo. 
 #O meu maior objetivo, sem dúvidas, é me tornar uma grande desenvolvedora.
 #Eu NUNCA vou desistir de ser uma Engenheira da Computação, boa em programação, e feliz pela profissão!
+#Prometo que voltarei em breve!
