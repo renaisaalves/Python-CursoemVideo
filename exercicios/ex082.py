fa0add2e4dbf51842106f99c71d0d5f0964b6df1
Renaísa <renaisaalves@gmail.com>
2022-04-01 18:33:15 -0300

    Update ex082.py

--- exercicios/ex082.py
+++ exercicios/ex082.py
@@ -3,3 +3,4 @@
 #Eu queria dizer que dei uma pausa temporária nos exercícios de programa.
 #Não é porque não amo, é porque precisei abdicar de algumas coisas para conseguir alcançar um objetivo de médio prazo. 
 #O meu maior objetivo, sem dúvidas, é me tornar uma grande desenvolvedora.
+#Eu NUNCA vou desistir de ser uma Engenheira da Computação, boa em programação, e feliz pela profissão!
