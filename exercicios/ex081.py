67329ac5ceffb8bda38390286b86ae1de1d1ed3d
Renaísa <renaisaalves@gmail.com>
2022-04-04 08:19:33 -0300

    Update ex081.py

--- exercicios/ex081.py
+++ exercicios/ex081.py
@@ -3,4 +3,9 @@
 #ALGORITMO
 #1º pedir para o usuário informar vários números:
 #   - o usuário insere um número e logo em seguida o programa pergunta se ele quer continuar inserindo; 
-#
+#   - o programa pedir para o usuário informar quantos números ele vai querer inserir. E, após o usuário inserir esses números, o programa perguntar se o usuário quer inserir mais números; 
+
+cont = 0
+
+while resposta not in 'SIMS':
+    
