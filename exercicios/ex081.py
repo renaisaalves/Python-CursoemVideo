3a01567957b6477651d8b3de06dad9b6ad546fcf
Renaísa <renaisaalves@gmail.com>
2022-04-04 08:15:23 -0300

    Update ex081.py

--- exercicios/ex081.py
+++ exercicios/ex081.py
@@ -1,2 +1,6 @@
 #ex 081: Crie um programa que vai ler vários números e colocar em uma lista. Depois disso mostre: A) Quantos números foram digitados. B) A lista de valores, ordenada de forma decrescente. C) Se o valor 5 foi digitado e está ou não na lista.
 
+#ALGORITMO
+#1º pedir para o usuário informar vários números:
+#   - o usuário insere um número e logo em seguida o programa pergunta se ele quer continuar inserindo; 
+# 
