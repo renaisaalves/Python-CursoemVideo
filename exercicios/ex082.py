7d7de19318125ce2828bc4d050d812f7812734a0
Renaísa <renaisaalves@gmail.com>
2022-04-13 18:34:39 -0300

    Update ex082.py

--- exercicios/ex082.py
+++ exercicios/ex082.py
@@ -1,14 +1,1 @@
 #ex082: 
-
-#Eu queria dizer que dei uma pausa temporária nos exercícios de programação.
-#Não é porque não amo, é porque precisei abdicar de algumas coisas para conseguir alcançar um objetivo de médio prazo. 
-#O meu maior objetivo, sem dúvidas, é me tornar uma grande desenvolvedora.
-#Eu NUNCA vou desistir de ser uma Engenheira da Computação, boa em programação, e feliz pela profissão!
-#Prometo que voltarei em breve!
-
-teste = []
-
-for c in range(0, 6):
-    teste.append(int(input('Digite um número: ')))
-print(f'Lista original: {teste}')
-print(f'Lista invertida: {teste.sort()}')
