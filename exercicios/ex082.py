5e131fa48c6a4bc3f3e8afddd24cc8fd151bb307
Renaísa <renaisaalves@gmail.com>
2022-04-14 18:40:13 -0300

    Update ex082.py

--- exercicios/ex082.py
+++ exercicios/ex082.py
@@ -1,14 +1,15 @@
 #ex082: Exercício Python 082: Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas.
 
-generico = []
+generico = pares = impares = []
 resposta = 'SIMSSS'
 
 while resposta in 'SIMSSS':
     generico.append(int(input('Digite um número: ')))
     resposta = str(input('Quer continuar? [S/N]: ')).strip().upper()
     if resposta not in 'SIMSSS':
-        if generico[0:] % 2 == 0: 
-            
+        for c, v in enumerate(generico):
+            if c % 2 == 0:
+                pares = v
         break
 print(f'{generico}')
 print(f'{pares}')
