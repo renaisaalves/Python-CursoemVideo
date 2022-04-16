ca0665c957113da0f269e1b46db62f848507caab
Renaísa <renaisaalves@gmail.com>
2022-04-16 09:10:33 -0300

    Update ex083.py

--- exercicios/ex083.py
+++ exercicios/ex083.py
@@ -1,4 +1,7 @@
 #Exercício Python 083: Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
 
 frase = str(input('Digite uma frase que use parênteses.\nFrase: '))
-print(frase)
+if '()' in frase:
+    print('Tem parênteses em sua frase.')
+else:
+    print('Não tem parênteses na sua frase.')
