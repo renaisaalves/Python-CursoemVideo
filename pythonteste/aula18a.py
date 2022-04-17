02865c9352cd348ee161d824b2251bdd95473d30
Renaísa <renaisaalves@gmail.com>
2022-04-17 13:13:04 -0300

    Update aula18a.py

--- pythonteste/aula18a.py
+++ pythonteste/aula18a.py
@@ -31,3 +31,20 @@ teste[1] = 22
 galera.append(teste[:])
 print(galera)
 
+for p in galera:
+    print(f'{p[0]} tem {p[1]} anos de idade.')
+    
+grupo = []
+dado = list()
+for c in range(0, 3):
+    dado.append(str(input('Nome: ')))
+    dado.append(int(input('Idade: ')))
+    galera.append(dado[:])
+    dado.clear() #clear limpa os dados
+print(galera)
+
+for p in grupo:
+    if p[1] >= 21:
+        print(f'{p[0]} é maior de idade.')
+    else:
+        print(f'{p[0]} é menor de idade.')
