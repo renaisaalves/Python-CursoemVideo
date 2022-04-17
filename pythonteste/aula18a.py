0e37d11173dd89e770183760f8fa4898debd52f6
Renaísa <renaisaalves@gmail.com>
2022-04-17 07:23:22 -0300

    Create aula18a.py

---
+++ pythonteste/aula18a.py
@@ -0,0 +1,11 @@
+#AULA FEITA POR MIM
+
+#Vamos adicionar listas dentro de uma lista! 
+
+#LISTA CRIADA A PARTIR DOS DADOS INSERIDOS PELO USUÁRIO
+
+lista1 = list()
+lista2 = list()
+lista3 = []
+
+lista1.append(str(input('Nome: ')))
