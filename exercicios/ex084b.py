16476b00611b1700438a8fececa3caf92f7d9a0f
Renaísa <renaisaalves@gmail.com>
2022-04-18 17:23:04 -0300

    Update ex084b.py

--- exercicios/ex084b.py
+++ exercicios/ex084b.py
@@ -1,3 +1,4 @@
 #EXERCÍCIO 84 FEITO PELO GUSTAVO GUANABARA
 #UMA VERGONHA PORQUE NÃO CONSEGUI FAZER
 
+#ao invés de criar um contador, você pode mandar len(lista principal) que vai ler quantas pessoas foram contabilizadas.
