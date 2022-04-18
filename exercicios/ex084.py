3e9ffb16861025804e4bacbce1d974c2c2e018d6
Renaísa <renaisaalves@gmail.com>
2022-04-18 11:22:38 -0300

    Update ex084.py

--- exercicios/ex084.py
+++ exercicios/ex084.py
@@ -1,12 +1,11 @@
 #ex084: Faça um programa que leia nome e peso de várias pessoas, guardando tudo em uma lista. No final, mostre:A) Quantas pessoas foram cadastradas. B) Uma listagem com as pessoas mais pesadas. C) Uma listagem com as pessoas mais leves.
 
-cadastro = list([''], [''], [''], [''])
+cadastro = [[''], [''], [''], ['']]
 
-for i in cadastro:
+for i in range(cadastro):
     nome = str(input('Nome: '))
     peso = int(input('Peso: '))
     cadastro.append(nome)
     cadastro.append(peso)
-    break
 print(cadastro)
 print(cadastro[0][0])
