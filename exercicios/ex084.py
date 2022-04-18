38f220cc0fe000d5155df49e54de7fd1a3674d00
Renaísa <renaisaalves@gmail.com>
2022-04-18 16:40:49 -0300

    Update ex084.py

--- exercicios/ex084.py
+++ exercicios/ex084.py
@@ -15,7 +15,7 @@ while True:
         break
 
 for i in listagem:
-    if i[1] > i[1]:
+    if i[0][1] > i[1]:
         print(f'{i[0]} é o mais pesado.')
     else:
         print(f'{i[0]} é o mais leve.')
