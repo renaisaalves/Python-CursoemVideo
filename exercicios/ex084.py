874cc5a621a0fa6e0016a4b9c3b126174e824ec3
Renaísa <renaisaalves@gmail.com>
2022-04-18 16:59:14 -0300

    Update ex084.py

--- exercicios/ex084.py
+++ exercicios/ex084.py
@@ -27,7 +27,5 @@ print('=' * 30)
 
 #ALGORITMO
 
-#Criamos duas listas (cadastro e listagem)
-#A primeira lista (cadastro) vai armazenar duas informações: nome e peso.
-#Quando eu coloco a primeira lista (cadastro) dentro de uma outra lista (listagem), estou adicionando ['Nome', peso], dessa forma: [['Nome', peso]]
-#Porém, eu LIMPO a lista cadastro. Dessa forma, ela vai armazenar novos valores.
+#Eu preciso fazer com que cada peso i[1] seja comparado com o outro valor. 
+
