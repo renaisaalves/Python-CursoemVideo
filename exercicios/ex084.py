664d29f86eff027e7cd1a9936cf32a4f32afe3aa
Renaísa <renaisaalves@gmail.com>
2022-04-18 15:34:49 -0300

    Update ex084.py

--- exercicios/ex084.py
+++ exercicios/ex084.py
@@ -1,16 +1,17 @@
 #ex084: Faça um programa que leia nome e peso de várias pessoas, guardando tudo em uma lista. No final, mostre:A) Quantas pessoas foram cadastradas. B) Uma listagem com as pessoas mais pesadas. C) Uma listagem com as pessoas mais leves.
 
 cadastro = []
+listagem = list()
 total = 0
 
 while True:
-    nome = str(input('Nome: ')).strip().capitalize()
-    peso = int(input('Peso: '))
-    cadastro.append(nome)
-    cadastro.append(peso)
+    cadastro.append(str(input('Nome: ')).capitalize())
+    cadastro.append(int(input('Peso: ')))
+    listagem.append(cadastro[:])
+    cadastro.clear()
     total += 1
     resposta = str(input('Quer continuar? [Sim/Não]: ')).upper()
     if resposta not in 'SIMS':
         break
-print(cadastro)
+print(listagem)
 print(f'{total} pessoas foram cadastradas.')
