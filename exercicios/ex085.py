1a657437aa228410327d1f20da5ed572befdec73
Renaísa <renaisaalves@gmail.com>
2022-04-19 18:27:13 -0300

    Update ex085.py

--- exercicios/ex085.py
+++ exercicios/ex085.py
@@ -1,3 +1,6 @@
 #085: Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
 
 valor = list()
+
+for i in range(0, 7):
+    valor.append(int(input('Digite o {}º valor: ')))
