c7650f6e70023d3d459565d8f38bd914fc2816c0
Renaísa <renaisaalves@gmail.com>
2022-04-21 14:44:31 -0300

    Update ex085.py

--- exercicios/ex085.py
+++ exercicios/ex085.py
@@ -7,9 +7,12 @@ e = '\033[m' #fechando as cores
 
 for i in range(1, 8):
     valor.append(int(input(f'Digite o {i}º valor: ')))
-for c in valor:
+for t, c in enumerate(valor):
     if c % 2 == 1:
         valor.remove(c)
         valor.append(c)
-print(f'Em amarelo representa todos os números {a}pares{e}.\n Em verde, todos os números {v}ímpares{e}.')
+print('=' * 30)
+print(f'Em amarelo representa todos os números {a} pares{e}.')
+print(f'Em verde, todos os números {v}ímpares{e}.')
+print('=' * 30)
 print(valor)
