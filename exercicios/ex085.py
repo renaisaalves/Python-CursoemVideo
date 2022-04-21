b477fc965a80a9f5a3b17a48d9ef9f6d4823763f
Renaísa <renaisaalves@gmail.com>
2022-04-21 16:50:06 -0300

    Update ex085.py

--- exercicios/ex085.py
+++ exercicios/ex085.py
@@ -1,18 +1,20 @@
 #085: Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
 
 valor = list()
-impar = list()
 par = list()
+impar = list()
 
 for i in range(1, 8):
     valor.append(int(input(f'Digite o {i}º valor: ')))
 valor.sort()
 
 for c in valor:
-    if c % 2 == 0:
-        par.append(c)
+    if c % 2 == 1:
+        impar.append(c)
         valor.remove(c)
-
-(print('=' * 30))
+        for h in impar:
+            valor.append(h)
+print(('=' * 30))
+print(f'Números ímpares: {impar}')
 print(f'Lista gerada: {valor}')
 (print('=' * 30))
