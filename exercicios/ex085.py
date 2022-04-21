e2b44bfc8e158f86f97b0a612c60ec26de22317d
Renaísa <renaisaalves@gmail.com>
2022-04-21 16:10:35 -0300

    Update ex085.py

--- exercicios/ex085.py
+++ exercicios/ex085.py
@@ -1,8 +1,7 @@
 #085: Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
 
-valor = list()
-a = '\033[1;33m' #a de amarelo
-v = '\033[1;32m' #v de  verde
+valor = list(int)
+impar = list(int)
 e = '\033[m' #fechando as cores
 
 for i in range(1, 8):
@@ -11,10 +10,9 @@ print(valor)
 
 for c in valor:
     if c % 2 == 1:
-        valor.append(c)
         valor.remove(c)
+        impar.append(c)
 print('=' * 30)
-print(f'Em amarelo representa todos os números {a}pares{e}.')
-print(f'Em verde, todos os números {v}ímpares{e}.')
+print(f'Números ímpares: {impar}.')
+print(f'Lista completa: {valor}')
 print('=' * 30)
-print(valor)
