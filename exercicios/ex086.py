37abbfc6d6eef24a9ed604684535a05abc021745
Renaísa <renaisaalves@gmail.com>
2022-04-22 15:58:32 -0300

    Update ex086.py

--- exercicios/ex086.py
+++ exercicios/ex086.py
@@ -1,3 +1,4 @@
 #Crie um programa que declare uma matriz de dimensão 3×3 e preencha com valores lidos pelo teclado. No final, mostre a matriz na tela, com a formatação correta.
 
-matriz = []
+m0 = [[], [], []]
+
