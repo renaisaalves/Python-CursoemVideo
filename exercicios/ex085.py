3d01ca5e76c6b84174c1d9cff3e9e01edcd6342f
Renaísa <renaisaalves@gmail.com>
2022-04-22 15:15:49 -0300

    Update ex085.py

--- exercicios/ex085.py
+++ exercicios/ex085.py
@@ -1,5 +1,8 @@
 #085: Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
 
+from sqlite3 import paramstyle
+
+
 valor = list()
 par = list()
 impar = list()
@@ -8,10 +11,10 @@ for i in range(1, 8):
     valor.append(int(input(f'Digite o {i}º valor: ')))
 valor.sort()
 for n in valor:
-    if n % 2 == 1:
-        impar.append(n)
-    else:
+    if n % 2 == 0:
         par.append(n)
+    else:
+        impar.append(n)
 valor.clear()
 valor.append(par)
 valor.append(impar)
