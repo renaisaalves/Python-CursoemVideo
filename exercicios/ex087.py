f68b1a383a025e53d82b4f851e1e2ddda4b85486
Renaísa <renaisaalves@gmail.com>
2022-04-25 17:11:45 -0300

    Update ex087.py

--- exercicios/ex087.py
+++ exercicios/ex087.py
@@ -4,7 +4,6 @@ a = '\033[1;33m'
 f = '\033[m'
 soma = []
 coluna3 = []
-linha2 = []
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
 for l in range(0, 3):
@@ -25,10 +24,11 @@ for p in matriz:
             par = sum(soma)
     coluna3.append(p[2])
     soma3 = sum(coluna3)
-    
+linha2 = max(matriz[1])
+
 print(f'\nA soma de todos os valores pares foi: {a}{par}{f} ({soma})')
 print(f'A soma dos valores da terceira coluna foi: {a}{soma3}{f} ({coluna3})')
-print(f'O maior valor da segunda linha foi: {a}esperança{f}')
+print(f'O maior valor da segunda linha foi: {a}{linha2}{f}')
 print('-=' * 30)
 
 #ALGORITMO PARA ENTENDER O PROGRAMA
