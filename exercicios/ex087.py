01b06de6d543eb9abc2f4ad3a211de92c3b8fba4
Renaísa <renaisaalves@gmail.com>
2022-04-25 16:14:18 -0300

    Update ex087.py

--- exercicios/ex087.py
+++ exercicios/ex087.py
@@ -1,6 +1,7 @@
 #ex087: Aprimore o desafio anterior, mostrando no final: A) A soma de todos os valores pares digitados. B) A soma dos valores da terceira coluna. C) O maior valor da segunda linha.
 
 a = '\033[1;33m'
+b = '\033[1;37m'
 f = '\033[m'
 soma = []
 
@@ -21,11 +22,12 @@ for p in matriz:
     for i in p:
         if i % 2 == 0:
             soma.append(i)
-print(soma)
+            par = sum(soma)
+print(par)
 
-print(f'\nA soma de todos os valores {a}pares{f} foi: {a}{f}')
-print(f'A soma dos valores da {a}terceira coluna{f} foi: {a}paz{f}')
-print(f'O maior valor da {a}segunda linha{f} foi: {a}esperança{f}')
+print(f'\nA soma de todos os valores {b}pares{f} foi: {a}{par}{f} ({soma})')
+print(f'A soma dos valores da {b}terceira coluna{f} foi: {a}paz{f}')
+print(f'O maior valor da {b}segunda linha{f} foi: {a}esperança{f}')
 print('-=' * 30)
 
 #ALGORITMO PARA ENTENDER O PROGRAMA
