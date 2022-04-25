25413e65e747ab8a60b95abf8c6a138cfca79228
Renaísa <renaisaalves@gmail.com>
2022-04-25 14:01:00 -0300

    Update ex087.py

--- exercicios/ex087.py
+++ exercicios/ex087.py
@@ -1,6 +1,7 @@
 #ex087: Aprimore o desafio anterior, mostrando no final: A) A soma de todos os valores pares digitados. B) A soma dos valores da terceira coluna. C) O maior valor da segunda linha.
 
 a = '\033[1;33m'
+r = '\033[1;36m'
 f = '\033[m'
 
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
@@ -15,6 +16,7 @@ for l in range(0, 3):
     for c in range(0, 3):
         print(f'[{matriz[l][c]:^5}]', end='')
     print() 
-print(f'\nA soma de todos os valores pares informados foi: {a}amor{f}')
+print(f'\nA soma de todos os valores {a}pares{f} foi: {a}amor{f}')
+print(f'A soma dos valores da terceira coluna foi: {r}paz{f}')
 print('-=' * 30)
 
