4ef8ae9510539e4a2edc5897d30d60fd61074ff1
Renaísa <renaisaalves@gmail.com>
2022-04-25 18:06:07 -0300

    Update ex088.py

--- exercicios/ex088.py
+++ exercicios/ex088.py
@@ -1,1 +1,4 @@
-#
+#ex088: Faça um programa que ajude um jogador da MEGA SENA a criar palpites. O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
+
+from random import randint 
+from time import sleep
