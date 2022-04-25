02b0aa2ef579241f70ac3f18d2076d2578f9ad63
Renaísa <renaisaalves@gmail.com>
2022-04-25 13:24:42 -0300

    Update ex087.py

--- exercicios/ex087.py
+++ exercicios/ex087.py
@@ -1,6 +1,7 @@
 #ex087: Aprimore o desafio anterior, mostrando no final: A) A soma de todos os valores pares digitados. B) A soma dos valores da terceira coluna. C) O maior valor da segunda linha.
 
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
 for l in range(0, 3):
     for c in range(0, 3):
         matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
