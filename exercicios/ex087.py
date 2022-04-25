0ea584de7318bfc4a4eb04d33e7bfb1229c14e5e
Renaísa <renaisaalves@gmail.com>
2022-04-25 13:56:46 -0300

    Update ex087.py

--- exercicios/ex087.py
+++ exercicios/ex087.py
@@ -7,9 +7,10 @@ for l in range(0, 3):
         matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
 print('-=' * 30)
 
-print('MATRIZ criada a partir dos valores digitados:')
+print('MATRIZ criada a partir dos valores digitados:\n')
 for l in range(0, 3):
     for c in range(0, 3):
         print(f'[{matriz[l][c]:^5}]', end='')
     print() 
+print('-=' * 30)
 
