c6dfbea6b57daa3e439f6204703d6ed3bc5e6a13
Renaísa <renaisaalves@gmail.com>
2022-04-27 18:16:32 -0300

    Update ex088.py

--- exercicios/ex088.py
+++ exercicios/ex088.py
@@ -2,3 +2,20 @@
 
 from random import randint 
 from time import sleep
+
+jogos = []
+print('=' * 50)
+print(f'{"BOLÃO DA MEGA SENA":^50}')
+print('=' * 50)
+num = int(input('Quantos jogos você quer que eu sorteie?\nRESPOSTA: '))
+print('~' * 50)
+for c in range(1, num + 1):
+    if c > 0:
+        jogos.clear()
+    for i in range(0, 7):
+        sorteio = randint(1, 60)
+        jogos.append(sorteio)
+        jogos.sort()
+        sleep(0.2)
+    print(f'{c}º jogo: {jogos}')
+print('~' * 50)
