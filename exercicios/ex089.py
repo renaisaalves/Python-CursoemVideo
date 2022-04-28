a2dfe41a3f06f0d3e4fc38ce631e7049627ea567
Renaísa <renaisaalves@gmail.com>
2022-04-28 17:38:38 -0300

    Update ex089.py

--- exercicios/ex089.py
+++ exercicios/ex089.py
@@ -1,5 +1,5 @@
 #ex089: Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta. No final, mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as notas de cada aluno individualmente.
 
 print('='* 40)
-print(f'{"BOLETIM ESCOLAR"}:^5')
+print(f'{"BOLETIM ESCOLAR":^40}')
 print('='* 40)
