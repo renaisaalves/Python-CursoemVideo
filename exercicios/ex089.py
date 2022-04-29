585445c5dda265a0490f4797f21f3b63d0dc3e0e
Renaísa <renaisaalves@gmail.com>
2022-04-29 16:41:24 -0300

    Update ex089.py

--- exercicios/ex089.py
+++ exercicios/ex089.py
@@ -10,12 +10,14 @@ while True:
     nome = str(input('Nome: ')).capitalize()
     nota1 = float(input('Nota 1: '))
     nota2 = float(input('Nota 2: '))
+    print('-' * 20)
     aluno.append(nome)
     aluno.append(nota1)
     aluno.append(nota2)
     cadastro.append(aluno[:])
     aluno.clear()
     resposta = str(input('Quer continuar? ')).upper()
+    print('-' * 20)
     if resposta not in 'SIMS':
         for c in cadastro:
             media = (c[1] + c[2]) / 2
