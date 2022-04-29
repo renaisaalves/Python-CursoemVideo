0d1d1f3f15a4165766867afd8956942e7d86f024
Renaísa <renaisaalves@gmail.com>
2022-04-29 15:26:02 -0300

    Update ex089.py

--- exercicios/ex089.py
+++ exercicios/ex089.py
@@ -1,5 +1,22 @@
 #ex089: Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta. No final, mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as notas de cada aluno individualmente.
 
+aluno = []
+cadastro = list()
+
 print('='* 40)
 print(f'{"BOLETIM ESCOLAR":^40}')
 print('='* 40)
+while True:
+    nome = str(input('Nome: ')).capitalize()
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    aluno.append(nome)
+    aluno.append(nota1)
+    aluno.append(nota2)
+    cadastro.append(aluno[:])
+    aluno.clear()
+    resposta = str(input('Quer continuar? '))
+    if resposta not in 'SIMS':
+        pass
+    break
+print(cadastro)
