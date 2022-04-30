bde28ac2e52a216af92250540159d3c44199211b
Renaísa <renaisaalves@gmail.com>
2022-04-30 17:43:28 -0300

    Update ex089.py

--- exercicios/ex089.py
+++ exercicios/ex089.py
@@ -32,11 +32,8 @@ print(f'{"CONSULTA INDIVIDUAL":^40}\nVocê pode acessar as notas individualmente
 print('-' * 40)
 while True:
     num = int(input('Mostrar notas de qual aluno?\nNº: '))
-    if num != type(int):
-        print(f'Ops! Digite um número ao invés do nome. ')
-    else:
-        if cadastro[num]:
-            print(f'As notas de {cadastro[num][0]} são: {cadastro[num][1]} e {cadastro[num][2]}.')
+    if cadastro[num]:
+        print(f'As notas de {cadastro[num][0]} são: {cadastro[num][1]} e {cadastro[num][2]}.')
     if num == 999:
         break
 
