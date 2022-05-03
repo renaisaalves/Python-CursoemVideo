839055f3ad7e2c8c501ff15c0bfd11c0ede64ff0
Renaísa <renaisaalves@gmail.com>
2022-05-03 14:17:29 -0300

    Update aula19a.py

---
+++ pythonteste/aula19a.py
@@ -0,0 +1,21 @@
+#DICIONÁRIOS
+
+#Há duas maneiras de declarar um dicionário:
+variavel = dict()
+variavel = {}
+
+#Dentro do dicionário, eu posso adicionar elementos. Esses elementos são conhecidos como chaves (keys) e valores (values):
+variavel = {'nome':'Pedro', 'idade':25}
+print(variavel['nome'])
+#Vale lembrar que para declarar uma key (o nome de um índice), eu preciso colocar os dois pontos. 'Pedro' e 25 são os meus valores. Se eu der um print na tela, terei como retorno: Pedro.
+
+#E se eu quiser adicionar mais um item no meu dicionário? Simples. Basta eu inserir:
+variavel['sexo']='M'
+#O que está dentro do colchetes é a minha key/índice, já o que está pra fora é o meu valor. 
+# Eu também posso deletar um item do meu dicionário, por exemplo:
+del variavel['idade']
+
+#ESTRUTURA DE REPETIÇÃO
+
+#Vamos imaginar que eu
+
