9a3ee26f95fa4790f3101e1bc75a6299a200ee86
Renaísa <renaisaalves@gmail.com>
2022-05-04 15:10:13 -0300

    Update aula19a.py

--- pythonteste/aula19a.py
+++ pythonteste/aula19a.py
@@ -51,7 +51,7 @@ for k in pessoas.values():
     
 del pessoas['sexo']
 #pessoas['nome':] = ['Katerine']
-pessoas['peso':] = [60]
+#pessoas['peso':] = [60]
 for k, v in pessoas.items():
     print(f'{k} = {v}')
     
