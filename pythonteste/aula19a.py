51b57654179ba6dc576c840e23ad6bb875c2b753
Renaísa <renaisaalves@gmail.com>
2022-05-04 15:09:09 -0300

    Update aula19a.py

--- pythonteste/aula19a.py
+++ pythonteste/aula19a.py
@@ -55,4 +55,13 @@ pessoas['peso':] = [60]
 for k, v in pessoas.items():
     print(f'{k} = {v}')
     
-
+estado = dict()
+brasil = list()
+for c in range(0, 3):
+    estado['uf'] = str(input('Unidade Federativa: '))
+    estado['sigla'] = str(input('Sigla do Estado: ')).upper()
+    brasil.append(estado.copy())
+for e in brasil:
+    for v in e.values():
+        print(v, end='')
+    print()
