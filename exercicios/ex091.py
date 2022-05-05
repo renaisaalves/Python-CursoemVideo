957e2c843b7c0aa699fbe568e67db12d2ca99e70
Renaísa <renaisaalves@gmail.com>
2022-05-05 17:23:58 -0300

    Update ex091.py

--- exercicios/ex091.py
+++ exercicios/ex091.py
@@ -1,2 +1,7 @@
 #091: Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um dicionário em Python. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
 
+from random import randint
+jogador = list()
+
+for c in range(0, 4):
+    jogador.append()
