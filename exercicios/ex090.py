d5dcb6a711586addbdc2f98a481a08e436428c81
Renaísa <renaisaalves@gmail.com>
2022-05-05 13:16:51 -0300

    Update ex090.py

--- exercicios/ex090.py
+++ exercicios/ex090.py
@@ -1,6 +1,12 @@
 #ex090: Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. No final, mostre o conteúdo da estrutura na tela.
 
 nome = str(input('Nome: ')).capitalize()
-media = float(input('Média: '))
-dicionario = {'nome': nome, 'media': media}
+nota1 = float(input('Nota 1: '))
+nota2 = float(input('Nota 2: '))
+media = (nota1 + nota2) / 2
+if media > 5:
+    situação = 'Aprovado'
+else:
+    situação = 'Reprovado'
+dicionario = {'aluno': nome, 'media': media, 'situação': situação}
 print(dicionario)
