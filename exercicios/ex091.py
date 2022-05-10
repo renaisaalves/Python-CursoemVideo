d0e06c77e51fd8a15b0d24c60dcb98a15aabb7d4
Renaísa <renaisaalves@gmail.com>
2022-05-10 17:42:15 -0300

    Update ex091.py

--- exercicios/ex091.py
+++ exercicios/ex091.py
@@ -7,7 +7,7 @@ sorteados = list()
 for c in range(0, 4):
     num = randint(1, 6)
     sorteados.append(num)
-numeros = {'jogador1': sorteados[0], 'jogador2': sorteados[1], 'jogador3': sorteados[2], 'jogador4': sorteados[3]}
+numeros = {'jogador 1': sorteados[0], 'jogador 2': sorteados[1], 'jogador 3': sorteados[2], 'jogador 4': sorteados[3]}
 print(numeros)
 
 #Vou usar o laço for 
