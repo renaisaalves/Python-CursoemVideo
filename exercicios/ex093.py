544b40a734c9f3dc0c81475f1df123a3c00d5908
Renaísa <renaisaalves@gmail.com>
2022-05-22 18:12:42 -0300

    Update ex093.py

--- exercicios/ex093.py
+++ exercicios/ex093.py
@@ -1,11 +1,14 @@
 #ex093:  Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
 
 dados = dict()
+gols = list()
 total = 0
 
-jogador = str(input('Nome: ')).capitalize()
+nome = str(input('Nome: ')).capitalize()
 partidas = int(input('Nº de partidas: '))
 for c in range(partidas):
-    gols = int(input(f'Quantos gols você fez na {c} partida? '))
-    total = total + gols
-dados = {'jogador': jogador, 'partidas': partidas, 'total de gols': total}
+    gol = int(input(f'Quantos gols você fez na {c} partida? '))
+    gols.append(gol)
+    total = total + gol
+dados = {'jogador': nome, 'gols': gols, 'total': total}
+
