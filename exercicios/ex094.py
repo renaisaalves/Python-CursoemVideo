5179b5b06002a187350d36d24654c907b5ac4c19
Renaísa <renaisaalves@gmail.com>
2022-05-24 14:27:16 -0300

    Update ex094.py

--- exercicios/ex094.py
+++ exercicios/ex094.py
@@ -1,8 +1,5 @@
 #ex094: Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um dicionário e todos os dicionários em uma lista. No final, mostre: A) Quantas pessoas foram cadastradas B) A média de idade C) Uma lista com as mulheres D) Uma lista de pessoas com idade acima da média.
 
-from doctest import NORMALIZE_WHITESPACE
-
-
 cadastro = dict()
 cadastros = list()
 while True:
