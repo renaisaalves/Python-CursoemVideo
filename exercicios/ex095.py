e700c5ba67357b250f4b907fc35f3e0a88a45be3
Renaísa <renaisaalves@gmail.com>
2022-05-26 13:43:35 -0300

    Update ex095.py

--- exercicios/ex095.py
+++ exercicios/ex095.py
@@ -1,1 +1,7 @@
 #ex095: Aprimore o desafio 93 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.
+
+dados = dict()
+gols = list()
+
+dados['jogador: '] = str(input('Nome: ')).capitalize()
+
