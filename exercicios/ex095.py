97647a2bc6768a8693a21d26854d2ecaa8cda7b5
Renaísa <renaisaalves@gmail.com>
2022-05-26 16:18:47 -0300

    Update ex095.py

--- exercicios/ex095.py
+++ exercicios/ex095.py
@@ -1,12 +1,15 @@
 #ex095: Aprimore o desafio 93 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.
 
 dados = dict()
+gols = list()
 total = list()
 
 while True:
     dados['jogador'] = str(input('Nome: ')).capitalize()
     dados['partidas'] = int(input('Nº de partidas: '))
-    dados['gols por partida'] 
+    for c in range(dados['partidas']):
+        print('não sei o que vai dar')
+    dados['gols'] = gols
     dados['total'] = sum(total)
     resp = str(input('Quer adicionar mais um jogador? [S/N]: '))[0].upper()
     if resp not in 'S':
