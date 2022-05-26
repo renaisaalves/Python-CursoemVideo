2ac5939144375d6aebf5b4490895a998ef7f8f9e
Renaísa <renaisaalves@gmail.com>
2022-05-26 16:13:41 -0300

    Update ex095.py

--- exercicios/ex095.py
+++ exercicios/ex095.py
@@ -1,8 +1,8 @@
 #ex095: Aprimore o desafio 93 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.
 
-#FIZ DO PROFESSOR
-
+dados = dict()
 while True:
+    dados['jogador'] = str(input('Nome: ')).capitalize()
     resp = str(input('Quer adicionar mais um jogador? [S/N]: '))[0].upper()
     if resp not in 'S':
         break
