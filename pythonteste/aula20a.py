a753ffd0c1703cb5322b46c731d0e26ba98f8401
Renaísa <renaisaalves@gmail.com>
2022-05-30 15:02:08 -0300

    Update aula20a.py

--- pythonteste/aula20a.py
+++ pythonteste/aula20a.py
@@ -30,3 +30,11 @@ def contador(* num):
 contador(2, 1, 7)
 contador(8, 0)
 contador(4, 4, 7, 6, 2)
+
+def contador(* num):
+    tam = len(num)
+    print(f'Recebi os valores {num} e são ao todo {tam}')
+
+contador(2, 1, 7)
+contador(8, 0)
+contador(4, 4, 7, 6, 2)
