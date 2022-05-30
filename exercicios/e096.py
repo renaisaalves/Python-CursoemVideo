6aa1c73b06747961309edcd1d837e25e8f28b410
Renaísa <renaisaalves@gmail.com>
2022-05-30 15:14:26 -0300

    Update e096.py

--- exercicios/e096.py
+++ exercicios/e096.py
@@ -1,1 +1,9 @@
 #Exercício Python 096: Faça um programa que tenha uma função chamada área(), que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno.
+
+def area(l, c):
+    total = l * c
+    print(total)
+    
+l = float(input('Largura: '))
+c = float(input('Comprimento: '))
+area(l, c)
