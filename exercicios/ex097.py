0c9a6f259a9d08c4d14860e0facbe5eb4c44020a
Renaísa <renaisaalves@gmail.com>
2022-05-31 15:48:17 -0300

    Update ex097.py

--- exercicios/ex097.py
+++ exercicios/ex097.py
@@ -1,7 +1,7 @@
 #ex097: Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.  
 
-def escreva():
-    print('=' * 20)
-    print(f'{"Hello, World!":^20}')
-    print('=' * 20)
-escreva()
+def escreva(msg):
+    print('=' * len(msg))
+    print(msg)
+    print('=' * len(msg))
+escreva('Bom dia, querido Dev!')
