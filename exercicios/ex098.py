0d6361483d035b76c372d5b24f7b592395ba0bc8
Renaísa <renaisaalves@gmail.com>
2022-06-02 18:28:46 -0300

    Update ex098.py

--- exercicios/ex098.py
+++ exercicios/ex098.py
@@ -2,20 +2,26 @@
 
 from time import sleep
 
-def contador(i, f):
+def contador(i, f, p):
     print('=' * 40)
-    print(f'Contagem de 1 até 10 [de 1 em 1]')
-    for i in range(1, 10+1):
-        print(f'{i} ', end='')
+    print(f'Contagem de 1 até 10 [de 1 em 1]:')
+    for c in range(1, 10+1):
+        print(f'{c} ', end='')
     print()
     print('=' * 40)
-    print(f'Contagem de 10 até 0 [de 2 em 2]')
-    for f in range(10, 0, -1):
-        if f % 2 == 0:
-            print(f'{f} ', end='')
+    print(f'Contagem de 10 até 0 [de 2 em 2]:')
+    for u in range(10, 0, -1):
+        if u % 2 == 0:
+            print(f'{u} ', end='')
     print()
     print('=' * 40)
-    print('Agora é a sua vez de realizar a contagem!')
+    for k in range(i, f, p):
+        print(f'Contagem personalizada:')
+        print(f'{k}', end='')
+        print()
 contador(0, 3)
-    
-    
+print('Agora é a sua vez de realizar a contagem!')
+i = int(input('Início: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+
