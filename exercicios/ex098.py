90002fc1df6b3b908c93ffc7d309fa00efe613d4
Renaísa <renaisaalves@gmail.com>
2022-06-02 18:38:14 -0300

    Update ex098.py

--- exercicios/ex098.py
+++ exercicios/ex098.py
@@ -1,4 +1,6 @@
 #ex098: Faça um programa que tenha uma função chamada contador(), que receba três parâmetros: início, fim e passo. Seu programa tem que realizar três contagens através da função criada:
 
-
+def contador(i, f, p):
+    pass
+contador(1, 10, 1)
 
