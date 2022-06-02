2b2576da784cf86e89f4efa477d70fa523e9d0cd
Renaísa <renaisaalves@gmail.com>
2022-06-02 18:52:02 -0300

    Update ex098.py

--- exercicios/ex098.py
+++ exercicios/ex098.py
@@ -1,18 +1,5 @@
 #ex098: Faça um programa que tenha uma função chamada contador(), que receba três parâmetros: início, fim e passo. Seu programa tem que realizar três contagens através da função criada:
 
-def contador(i, f, p):
-    print(f'Contagem de {i} até {f} de {p} em {p}:')
-    if i <= f:
-        cont = 1
-        while i <= f:
-            print(f'{cont} ', end='')
-            cont += p
-        print('FIM!')
-    else:
-        cont = 1
-        while cont >= f:
-            print(f'{cont} ', end='')
-            cont -= p
-        print('FIM!')
+
 contador(1, 10, 1)
 
