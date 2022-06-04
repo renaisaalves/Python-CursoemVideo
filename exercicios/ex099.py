2fe803258ed24f562054b74ed4a5c62a3d7d650e
Renaísa <renaisaalves@gmail.com>
2022-06-04 12:45:00 -0300

    Update ex099.py

--- exercicios/ex099.py
+++ exercicios/ex099.py
@@ -1,11 +1,5 @@
 #ex099: Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
 
 def maior(valor):
-    for c in range(0, 1):
-        mai = men = valor
-    if c >= 0:
-        print('Deu certo')
-num = 10    
-maior(num)
-
-
+    pass
+maior()
