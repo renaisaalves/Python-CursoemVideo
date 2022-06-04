b1e2aba6305d3a309a259ef3eacf9cf7e26390d6
Renaísa <renaisaalves@gmail.com>
2022-06-04 08:00:59 -0300

    Update ex099.py

--- exercicios/ex099.py
+++ exercicios/ex099.py
@@ -1,8 +1,10 @@
 #ex099: Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
 
+num = [3, 5, 10, 4, 2, 6]
+
 def maior(valor):
     valor.sort(reverse=True)
     print(f'O maior valor é {valor[0]}')
-maior([3, 5, 10, 4, 2, 6])
+maior(num)
 
 
