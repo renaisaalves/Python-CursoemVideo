fa17610f59ed53289aab5b4d35fcbc42259297b1
Renaísa <renaisaalves@gmail.com>
2022-06-04 07:50:46 -0300

    Update ex099.py

--- exercicios/ex099.py
+++ exercicios/ex099.py
@@ -1,12 +1,7 @@
 #ex099: Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
 
-def maior(values):
-    pass
-
-inteiros = list()
-while True:
-    inteiros.append(int(input('Número: ')))
-    resp = str(input('Quer adicionar mais algum número? [S/N]: ')).upper()[0]
-    if resp in 'N':
-        break
-print(inteiros)
+def maior(* valor):
+    tamanho = len(valor)
+    print(f'Recebi os valores {valor} e são ao todo {tamanho}')
+maior(3, 4, 8)
+maior(5, 2)
