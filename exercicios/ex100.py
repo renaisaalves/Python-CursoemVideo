7ac9659b00a9bee52b04efff1026e891b9b62e0b
Renaísa <renaisaalves@gmail.com>
2022-06-05 18:44:31 -0300

    Update ex100.py

--- exercicios/ex100.py
+++ exercicios/ex100.py
@@ -1,5 +1,8 @@
 #ex100: Faça um programa que tenha uma lista chamada números e duas funções chamadas sorteia() e somaPar(). A primeira função vai sortear 5 números e vai colocá-los dentro da lista e a segunda função vai mostrar a soma entre todos os valores pares sorteados pela função anterior.
 
-from 
+from random import randint
 def sorteia():
-    sorteados = randint()
+    sorteados = list()
+    for c in range(0, 6):
+        sorteados.append(randint(0, 100))
+    
