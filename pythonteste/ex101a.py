cac5ffc95fc39708230e41770d5854c84a056ba3
Renaísa <renaisaalves@gmail.com>
2022-06-12 07:46:06 -0300

    Update ex101a.py

--- pythonteste/ex101a.py
+++ pythonteste/ex101a.py
@@ -1,7 +1,7 @@
 #ex101: Crie um programa que tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições.
 
 def voto(year):
-    if year < 15:
+    if year <= 15:
         print(f'Voto NEGADO.')
     elif year >= 16 and year <= 17:
         print(f'Voto OPCIONAL.')
