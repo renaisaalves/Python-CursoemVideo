5ad1a5c0aebd6bd357701ef6baf0e4d7a2551490
Renaísa <renaisaalves@gmail.com>
2022-06-14 17:43:03 -0300

    Update ex102.py

--- exercicios/ex102.py
+++ exercicios/ex102.py
@@ -1,7 +1,7 @@
 #ex102: Crie um programa que tenha uma função fatorial() que receba dois parâmetros: o primeiro que indique o número a calcular e outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
 
-def fatorial(n, s=False):
-    if s == True:
+def fatorial(n, show=False):
+    if show == True:
         for c in range(n-1, 0, -1):
             if c == n-1:
                 fat = n * c
@@ -10,4 +10,4 @@ def fatorial(n, s=False):
                 fat *= c
                 print(f'x {c} ', end='')
     print(f'= {fat}')
-fatorial(4, show=True)
+fatorial(4, show=False)
