0a9d6b42b71512b996da5025fb5f5350219b3f94
Renaísa <renaisaalves@gmail.com>
2022-06-16 18:54:30 -0300

    Create ex105.py

---
+++ exercicios/ex105.py
@@ -0,0 +1,1 @@
+#ex105: Faça um programa que tenha uma função notas() que pode receber várias notas de alunos e vai retornar um dicionário com as seguintes informações: quantidade de notas; a maior nota; a menor nota; a média da turma; a situação (opcional)
