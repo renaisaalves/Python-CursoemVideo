bf686c68ed00a397d66966cdfa431e8ec2c2489e
Renaísa <renaisaalves@gmail.com>
2022-06-16 18:43:34 -0300

    Update ex104.py

--- exercicios/ex104.py
+++ exercicios/ex104.py
@@ -1,7 +1,7 @@
 #ex104: Crie um programa que tenha a função leiaInt(), que vai funcionar de forma semelhante ‘a função input() do Python, só que fazendo a validação para aceitar apenas um valor numérico. Ex: n = leiaInt(‘Digite um n: ‘)
 
 def leiaInt():
-    n = str(input('Digite um n: '))
+    n = str(input('Digite um número: '))
     if n.isnumeric() == True:
         print(f'Você acabou de digitar o número {n}')
     else:
