5764f0061b4eae87645fd620f34822bd7d5c5b1e
Renaísa <renaisaalves@gmail.com>
2022-06-17 10:44:57 -0300

    Update ex105.py

--- exercicios/ex105.py
+++ exercicios/ex105.py
@@ -3,9 +3,18 @@
 def notas(*valores):
     quant = len(valores)
     media = (sum(valores)) / quant
+    a = 0
     for c in valores:
+        if a == 0:
+            maior = menor = c
+            a += 1
+        else:
+            if c > maior:
+                maior = c
+            if c < menor:
+                menor = c
         print(f'Testando {c}')
-    boletim = {'quantidade de notas': quant, 'média': media}
+    boletim = {'quantidade de notas': quant, 'maior nota': maior, 'menor nota': menor, 'média': media}
     return boletim
 resp = notas(5.5, 8, 1.5)
 print(resp) 
