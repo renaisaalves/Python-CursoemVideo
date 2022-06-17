70563287a5a83a350e37e8bd2b62b578cead0606
Renaísa <renaisaalves@gmail.com>
2022-06-17 10:35:31 -0300

    Update ex105.py

--- exercicios/ex105.py
+++ exercicios/ex105.py
@@ -1,18 +1,6 @@
 #ex105: Faça um programa que tenha uma função notas() que pode receber várias notas de alunos e vai retornar um dicionário com as seguintes informações: quantidade de notas; a maior nota; a menor nota; a média da turma; a situação (opcional)
 
 def notas(*valores):
-    quant = len(valores)
-    dados = list(valores)
-    for c, i in dados:
-        if c == 0:
-            maior = menor = i
-        if c > 0:
-            if i > maior:
-                maior = i
-            if i < menor:
-                menor = i
-    boletim = {'quantidade de notas': quant, 'maior nota': maior, 'menor nota': menor}
-    return boletim
-resp = notas(5.5, 8, 1.5)
-print(resp)
-# sit=True
+    pass
+resp = notas(5.5, 8, 1.5, sit=True)
+print(resp) 
