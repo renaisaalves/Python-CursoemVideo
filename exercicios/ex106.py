df3217882d7719114b2b6deb990fcb45cb8676e3
Renaísa <renaisaalves@gmail.com>
2022-06-18 07:16:32 -0300

    Update ex106.py

--- exercicios/ex106.py
+++ exercicios/ex106.py
@@ -1,1 +1,7 @@
-#
+#ex106: Faça um mini-sistema que utilize o Interactive Help do Python. O usuário vai digitar o comando e o manual vai aparecer. Quando o usuário digitar a palavra ‘FIM’, o programa se encerrará. Importante: use cores
+
+def interactive_help(com):
+    help(com)
+
+comando = str(input('Digite um comando: '))
+interactive_help()
