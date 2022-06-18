1beed06ec25bd2a43fc6918827a5a52da0198b87
Renaísa <renaisaalves@gmail.com>
2022-06-18 15:08:59 -0300

    Update ex106.py

--- exercicios/ex106.py
+++ exercicios/ex106.py
@@ -1,8 +1,5 @@
 #ex106: Faça um mini-sistema que utilize o Interactive Help do Python. O usuário vai digitar o comando e o manual vai aparecer. Quando o usuário digitar a palavra ‘FIM’, o programa se encerrará. Importante: use cores
 
-def interactive_help(com):
-    help(com)
-    
 def colors(msg):
     red = print('\033[1;31m', msg, '\033[m')
     green = print('\033[1;32m', msg, '\033[m')
@@ -11,8 +8,10 @@ def colors(msg):
     purple = print('\033[1;35m', msg, '\033[m')
     cian = print('\033[1;36m', msg, '\033[m')
     white = print('\033[1;37m', msg, '\033[m')
-colors(interactive_help())
- 
+    
+def interactive_help(com):
+    help(com)
+    
 while True:
     comando = str(input('Digite um comando: '))
     if comando == 'fim':
