1583b9e867f6830eb1a984c5d34a59653cffe7be
Renaísa <renaisaalves@gmail.com>
2022-06-19 16:52:08 -0300

    Update ex106.py

--- exercicios/ex106.py
+++ exercicios/ex106.py
@@ -4,13 +4,14 @@ def interactive_help(com):
     help(com)
     
 def colors(msg):
-    red = print('\033[1;31m', msg, '\033[m')
-    green = print('\033[1;32m', msg, '\033[m')
-    yellow = print('\033[1;33m', msg, '\033[m')
-    blue = print('\033[1;34m', msg, '\033[m')
-    purple = print('\033[1;35m', msg, '\033[m')
-    cian = print('\033[1;36m', msg, '\033[m')
-    white = print('\033[1;37m', msg, '\033[m')
+    for c in range(0, 7):
+        red = print('\033[1;31m', msg, '\033[m')
+        green = print('\033[1;32m', msg, '\033[m')
+        yellow = print('\033[1;33m', msg, '\033[m')
+        blue = print('\033[1;34m', msg, '\033[m')
+        purple = print('\033[1;35m', msg, '\033[m')
+        cian = print('\033[1;36m', msg, '\033[m')
+        white = print('\033[1;37m', msg, '\033[m')
 
 while True:
     comando = str(input('Digite um comando: '))
