18cb896f3525ede3d99d0f17029665751d413941
Renaísa <renaisaalves@gmail.com>
2022-06-25 17:45:19 -0300

    Updates

--- pythonteste/aula22a.py
+++ pythonteste/aula22a.py
@@ -1,14 +1,3 @@
-def fatorial(n):
-    f = 1
-    for c in range(1, n+1):
-        f*=c
-    return f
-
-def dobro(n):
-    return n*2
-
-def triplo(n):
-    return n*3
 
 num = int(input('Digite um valor: '))
 fat = fatorial(num)
